7e0c95f2bb3b17e5270071c938372871886a4b63
Angel Marcel <angelmarcel364@gmail.com>
2025-01-02 19:02:57 +0300

    class person

---
+++ school.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Person
+{
+public:
+    std::string name;
+    int age;
+
+    Person(std::string n, int a) : name(n), age(a) {}
+};
