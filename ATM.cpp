c092ed06a06ae805672c7e88aebeafc71664278b
Angel Marcel <angelmarcel364@gmail.com>
2025-01-02 18:23:26 +0300

    with draw function

--- ATM.cpp
+++ ATM.cpp
@@ -9,9 +9,22 @@ private:
 
 public:
     ATM(std::string initialPin, double initialBalance) : pin(initialPin), balance(initialBalance) {}
-    
+
     void changePin(std::string newPin)
     {
         pin = newPin;
         std::cout << "PIN changed successfully." << std::endl;
+    }
+
+    void withdraw(double amount)
+    {
+        if (amount > balance)
+        {
+            std::cout << "Insufficient balance." << std::endl;
+        }
+        else
+        {
+            balance -= amount;
+            std::cout << "Withdrawal successful. New balance: $" << balance << std::endl;
+        }
     }
